7834faab4dcf98649ec75ceb0af9e073de4f72d3
Mitaka00 <mitko_a2000@abv.bg>
2017-06-13 21:39:05 +0300

    Lists Lab

---
+++ 18-Lists Lab/Square Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            var squareNumbers = new List<int>();
+
+            foreach (var num in numbers)
+            {
+                if(Math.Sqrt(num)==(int)Math.Sqrt(num))
+                {
+                    squareNumbers.Add(num);
+                }
+            }
+
+            squareNumbers.Sort();
+            squareNumbers.Reverse();
+
+            Console.WriteLine(string.Join(" ",squareNumbers));
+        }
+    }
+}
