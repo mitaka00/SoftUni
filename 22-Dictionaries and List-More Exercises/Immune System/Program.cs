25f1900bd0bb8140ca461364e7e8de1f9c251a3c
Mitaka00 <mitko_a2000@abv.bg>
2017-06-18 17:30:24 +0300

    Exercisese for dictionaries

---
+++ 22-Dictionaries and List-More Exercises/Immune System/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Immune_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startedHealth = int.Parse(Console.ReadLine());
+            var virusname = Console.ReadLine();
+            var virusList = new List<string>();
+
+            var health = startedHealth;
+            var timeToDefeat = 0;
+           
+            while(virusname!="end")
+            {
+                var virusStrength = virusname.Sum(x => x) / 3;
+                
+              if(!virusList.Contains(virusname))
+                {
+                    virusList.Add(virusname);
+                    timeToDefeat = virusStrength * virusname.Length;
+                }
+              else
+                {
+                    timeToDefeat = (virusStrength * virusname.Length)/3;
+                }
+
+                Console.WriteLine($"Virus {virusname}: {virusStrength} => {timeToDefeat} seconds");
+
+                if(health>timeToDefeat)
+                {
+                    health -= timeToDefeat;
+                    var seconds = timeToDefeat % 60;
+                    var min = timeToDefeat / 60;
+
+                    Console.WriteLine($"{virusname} defeated in {min}m {seconds}s.");
+                    Console.WriteLine($"Remaining health: {health}");
+                 
+                    if(health+(int)(0.2*health)>startedHealth)
+                    {
+                        health = startedHealth;
+                    }
+                    else
+                    {
+                        health += (int)(0.2 * health);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Immune System Defeated.");
+                    return;
+                }
+                virusname=Console.ReadLine();
+            }
+            Console.WriteLine($"Final Health: {health}");
+        }
+    }
+}
---
+++ 22-Dictionaries and List-More Exercises/SuperMarket Database/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket_Database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inPut = Console.ReadLine();
+
+            var productPriceQuantity = new Dictionary<string, Dictionary<double, int>>();
+            var productQuantity = new Dictionary<string, int>();
+            var subTotal = 0.0;
+
+            while(inPut!="stocked")
+           {
+                var SplittedInPut=inPut.Split(' ').ToList();
+
+                var product = SplittedInPut[0];
+                var price = double.Parse(SplittedInPut[1]);
+                var quantity = int.Parse(SplittedInPut[2]);
+
+                if(!productPriceQuantity.ContainsKey(product))
+                {
+                    productPriceQuantity[product] = new Dictionary<double, int>();
+                    productPriceQuantity[product].Add(price,quantity);
+
+                    productQuantity[product] = quantity;
+                }
+                else
+                {
+                    quantity += productQuantity[product];
+                    productQuantity[product] = quantity;
+
+                    productPriceQuantity[product].Clear();
+                    productPriceQuantity[product].Add(price, quantity);
+                }
+
+                inPut=Console.ReadLine();
+           }
+
+            foreach (var product in productPriceQuantity)
+            {
+                foreach (var priceQuant in product.Value)
+                {
+                    var sumForOneProduct = priceQuant.Key*priceQuant.Value;
+                    subTotal += sumForOneProduct;
+
+                    Console.WriteLine(
+                        $"{product.Key}: ${priceQuant.Key:f2} * {priceQuant.Value} = ${sumForOneProduct:f2}");
+                }
+            }
+
+            Console.WriteLine("------------------------------");
+            Console.WriteLine($"Grand Total: ${subTotal:f2}");
+        }
+    }
+}
