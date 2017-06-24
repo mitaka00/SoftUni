c8b5a35e5f70c575a3d1b0e21a610b65a7bda309
Mitaka00 <mitko_a2000@abv.bg>
2017-06-24 14:11:07 +0300

    fix exercise

---
+++ 24-Object and Classes-Exercises/Book Library Motification/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Book_Library_Motification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var bookInfo = new Dictionary<string, DateTime>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split().ToList();
+
+                var title = input[0];
+                DateTime date =DateTime.ParseExact(input[3], "dd.MM.yyyy", CultureInfo.InvariantCulture);
+
+                bookInfo[title] = date;
+
+            }
+
+            var secondinput = Console.ReadLine();
+            DateTime startdate = DateTime.ParseExact(secondinput, "dd.MM.yyyy", CultureInfo.InvariantCulture);
+
+            foreach (var date in bookInfo.OrderBy(x=>x.Value).ThenBy(x=>x.Key))
+            {
+                if(startdate<date.Value)
+                {
+                    Console.WriteLine($"{date.Key} -> {date.Value:dd.MM.yyyy}");
+                }
+            }
+        }
+    }
+ }
+
