33d1416c6f52225b6edbc92cc37cdd675f362d30
Mitaka00 <mitko_a2000@abv.bg>
2017-06-27 23:45:03 +0300

    String and Text Processing-Lab

---
+++ 28-String and Text Processing-Lab/Count Substring Ocurrences/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_Substring_Ocurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine().ToLower();
+            var specialWord = Console.ReadLine().ToLower();
+
+            var countWord = 0;
+            var index = text.IndexOf(specialWord);
+
+            while(index!=-1)
+            {
+                countWord++;
+                index = text.IndexOf(specialWord, index + 1);
+                    
+            }
+
+            Console.WriteLine(countWord);
+        }
+    }
+}
