1a4282ac54e186622d9697338d125d3cb78ed97d
Mitaka00 <mitko_a2000@abv.bg>
2017-07-01 15:10:15 +0300

    Update

---
+++ Programing Fundamentals/30-RegEx-Lab/Match Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Match_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var pattern = @"(^|(?<=\s))-?\d+(\.\d+)?($|(?=\s))";
+
+            var matchedNumbers = Regex.Matches(input, pattern);
+
+            foreach (Match number in matchedNumbers)
+            {
+                Console.Write(number+ " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
