1e3117c4ab83052ed2a60c112ab37be94dbd04f8
Mitaka00 <mitko_a2000@abv.bg>
2017-11-20 08:12:22 +0200

    JS Libraries

---
+++ Front-End Development/JS FOR FRONT-END/JS Libraries/Simple Accordion/simpleAccordion.js
@@ -0,0 +1,15 @@
+function solve() {
+    $('.toggle').click(function (e) {
+        e.preventDefault();
+        var $this = $(this);
+        if ($this.next().hasClass('show')) {
+            $this.next().removeClass('show');
+            $this.next().slideUp(350);
+        } else {
+            $this.parent().parent().find('li .inner').removeClass('show');
+            $this.parent().parent().find('li .inner').slideUp(350);
+            $this.next().toggleClass('show');
+            $this.next().slideToggle(350);
+        }
+    });
+}
